25addfc0735ed4ec227adb7e6327e0045c19cea5
magmacrunchmedia <magmacrunchmedia@gmail.com>
2026-07-10 21:02:09 -0400

    feat(chess): improve pixel art chess pieces with shading
    - Added shadeColor helper for 3D lighting effect
    - King: detailed crown with 5 prongs, cross, gem, tapered body
    - Queen: elegant crown with 7 prongs, globe+cross, ornate
    - Rook: castle with battlements, arrow slit windows, ledge
    - Bishop: mitre with slit, collar gems, tapered body
    - Knight: horse head with ears, mane, snout, nostril, detailed eye
    - Pawn: rounded head, collar, tapered body with highlights

--- arcade/chess/js/main.js
+++ arcade/chess/js/main.js
@@ -279,102 +279,343 @@ var UI = (function() {
     }
 
     function drawKing(ctx, color) {
-        // Base
+        // Shadow color
+        var dark = shadeColor(color, -40);
+        var light = shadeColor(color, 40);
+
+        // Base platform
+        ctx.fillStyle = dark;
+        ctx.fillRect(7, 28, 18, 3);
         ctx.fillStyle = color;
-        ctx.fillRect(8, 26, 16, 4);
-        // Stem
-        ctx.fillRect(12, 18, 8, 8);
-        // Crown
-        ctx.fillRect(6, 12, 20, 6);
-        // Crown points
-        ctx.fillRect(6, 8, 2, 4);
-        ctx.fillRect(11, 8, 2, 4);
-        ctx.fillRect(15, 8, 2, 4);
-        ctx.fillRect(20, 8, 2, 4);
+        ctx.fillRect(8, 27, 16, 3);
+
+        // Body - tapered column
+        ctx.fillStyle = color;
+        ctx.fillRect(11, 17, 10, 10);
+        ctx.fillStyle = dark;
+        ctx.fillRect(11, 17, 2, 10);
+        ctx.fillRect(19, 17, 2, 10);
+        ctx.fillStyle = light;
+        ctx.fillRect(14, 17, 3, 10);
+
+        // Waist/collar
+        ctx.fillStyle = dark;
+        ctx.fillRect(10, 16, 12, 2);
+
+        // Crown band
+        ctx.fillStyle = color;
+        ctx.fillRect(9, 12, 14, 4);
+        ctx.fillStyle = dark;
+        ctx.fillRect(9, 12, 14, 1);
+
+        // Crown points (5 prongs)
+        ctx.fillStyle = color;
+        ctx.fillRect(9, 8, 2, 4);
+        ctx.fillRect(13, 6, 2, 6);
+        ctx.fillRect(17, 6, 2, 6);
+        ctx.fillRect(21, 8, 2, 4);
+
+        // Crown prong tips (gems)
+        ctx.fillStyle = light;
+        ctx.fillRect(9, 7, 2, 2);
+        ctx.fillRect(13, 5, 2, 2);
+        ctx.fillRect(17, 5, 2, 2);
+        ctx.fillRect(21, 7, 2, 2);
+
         // Cross on top
-        ctx.fillRect(14, 4, 4, 6);
-        ctx.fillRect(12, 6, 8, 2);
-        // Gems
+        ctx.fillStyle = color;
+        ctx.fillRect(15, 1, 2, 6);
+        ctx.fillRect(13, 3, 6, 2);
+        ctx.fillStyle = light;
+        ctx.fillRect(15, 1, 2, 1);
+        ctx.fillRect(13, 3, 1, 2);
+
+        // Gem in center
         ctx.fillStyle = '#ffffff';
-        ctx.fillRect(15, 14, 2, 2);
+        ctx.fillRect(16, 13, 1, 1);
     }
 
     function drawQueen(ctx, color) {
-        // Base
+        var dark = shadeColor(color, -40);
+        var light = shadeColor(color, 40);
+
+        // Base platform
+        ctx.fillStyle = dark;
+        ctx.fillRect(7, 28, 18, 3);
         ctx.fillStyle = color;
-        ctx.fillRect(6, 26, 20, 4);
-        // Stem
-        ctx.fillRect(10, 18, 12, 8);
-        // Crown
-        ctx.fillRect(6, 12, 20, 6);
-        // Crown points
-        ctx.fillRect(6, 6, 3, 6);
-        ctx.fillRect(14, 4, 4, 8);
-        ctx.fillRect(23, 6, 3, 6);
-        // Gems
+        ctx.fillRect(8, 27, 16, 3);
+
+        // Body - elegant taper
+        ctx.fillStyle = color;
+        ctx.fillRect(11, 18, 10, 9);
+        ctx.fillStyle = dark;
+        ctx.fillRect(11, 18, 2, 9);
+        ctx.fillRect(19, 18, 2, 9);
+        ctx.fillStyle = light;
+        ctx.fillRect(14, 18, 3, 9);
+
+        // Waist
+        ctx.fillStyle = dark;
+        ctx.fillRect(10, 17, 12, 2);
+
+        // Crown band
+        ctx.fillStyle = color;
+        ctx.fillRect(9, 13, 14, 4);
+        ctx.fillStyle = dark;
+        ctx.fillRect(9, 13, 14, 1);
+
+        // Crown points (7 prongs, alternating tall/short)
+        ctx.fillStyle = color;
+        ctx.fillRect(8, 8, 2, 5);
+        ctx.fillRect(11, 5, 2, 8);
+        ctx.fillRect(14, 3, 2, 10);
+        ctx.fillRect(17, 5, 2, 8);
+        ctx.fillRect(20, 8, 2, 5);
+
+        // Crown tips with jewels
+        ctx.fillStyle = light;
+        ctx.fillRect(8, 7, 2, 2);
+        ctx.fillRect(11, 4, 2, 2);
+        ctx.fillRect(14, 2, 2, 2);
+        ctx.fillRect(17, 4, 2, 2);
+        ctx.fillRect(20, 7, 2, 2);
+
+        // Central orb (globe + cross)
+        ctx.fillStyle = color;
+        ctx.fillRect(15, 0, 2, 4);
+        ctx.fillRect(14, 2, 4, 2);
+
+        // Gems in crown band
         ctx.fillStyle = '#ffffff';
-        ctx.fillRect(15, 14, 2, 2);
+        ctx.fillRect(12, 14, 1, 1);
+        ctx.fillRect(16, 14, 1, 1);
+        ctx.fillRect(20, 14, 1, 1);
     }
 
     function drawRook(ctx, color) {
-        // Base
+        var dark = shadeColor(color, -40);
+        var light = shadeColor(color, 40);
+
+        // Base platform
+        ctx.fillStyle = dark;
+        ctx.fillRect(6, 28, 20, 3);
         ctx.fillStyle = color;
-        ctx.fillRect(8, 26, 16, 4);
-        // Body
-        ctx.fillRect(10, 12, 12, 14);
-        // Battlements
+        ctx.fillRect(7, 27, 18, 3);
+
+        // Main body
+        ctx.fillStyle = color;
+        ctx.fillRect(9, 13, 14, 14);
+        ctx.fillStyle = dark;
+        ctx.fillRect(9, 13, 2, 14);
+        ctx.fillRect(21, 13, 2, 14);
+        ctx.fillStyle = light;
+        ctx.fillRect(13, 13, 3, 14);
+
+        // Arrow slit windows
+        ctx.fillStyle = dark;
+        ctx.fillRect(14, 16, 4, 1);
+        ctx.fillRect(14, 20, 4, 1);
+
+        // Upper ledge
+        ctx.fillStyle = dark;
+        ctx.fillRect(7, 12, 18, 2);
+
+        // Battlements (crenellation)
+        ctx.fillStyle = color;
         ctx.fillRect(6, 6, 20, 6);
-        // Notches
-        ctx.fillRect(8, 6, 2, 3);
-        ctx.fillRect(14, 6, 4, 3);
-        ctx.fillRect(22, 6, 2, 3);
+        ctx.fillStyle = dark;
+        ctx.fillRect(6, 6, 20, 1);
+
+        // Battlement gaps
+        ctx.fillStyle = '#0d0820';
+        ctx.fillRect(8, 8, 3, 4);
+        ctx.fillRect(14, 8, 4, 4);
+        ctx.fillRect(21, 8, 3, 4);
+
+        // Top edge highlights
+        ctx.fillStyle = light;
+        ctx.fillRect(6, 6, 20, 1);
     }
 
     function drawBishop(ctx, color) {
-        // Base
+        var dark = shadeColor(color, -40);
+        var light = shadeColor(color, 40);
+
+        // Base platform
+        ctx.fillStyle = dark;
+        ctx.fillRect(7, 28, 18, 3);
         ctx.fillStyle = color;
-        ctx.fillRect(8, 26, 16, 4);
+        ctx.fillRect(8, 27, 16, 3);
+
         // Body
-        ctx.fillRect(11, 16, 10, 10);
-        // Pointed top
-        ctx.fillRect(12, 10, 8, 6);
-        ctx.fillRect(13, 6, 6, 4);
-        ctx.fillRect(14, 2, 4, 4);
-        // Cross on top
+        ctx.fillStyle = color;
+        ctx.fillRect(11, 18, 10, 9);
+        ctx.fillStyle = dark;
+        ctx.fillRect(11, 18, 2, 9);
+        ctx.fillRect(19, 18, 2, 9);
+        ctx.fillStyle = light;
+        ctx.fillRect(14, 18, 3, 9);
+
+        // Collar
+        ctx.fillStyle = dark;
+        ctx.fillRect(10, 17, 12, 2);
+
+        // Mitre (pointed hat) - main
+        ctx.fillStyle = color;
+        ctx.fillRect(11, 10, 10, 7);
+        ctx.fillStyle = dark;
+        ctx.fillRect(11, 10, 2, 7);
+        ctx.fillRect(19, 10, 2, 7);
+        ctx.fillStyle = light;
+        ctx.fillRect(14, 10, 3, 7);
+
+        // Mitre - upper
+        ctx.fillStyle = color;
+        ctx.fillRect(12, 6, 8, 4);
+        ctx.fillRect(13, 4, 6, 2);
+        ctx.fillRect(14, 2, 4, 2);
+        ctx.fillRect(15, 0, 2, 2);
+
+        // Mitre slit (distinguishing feature)
+        ctx.fillStyle = dark;
+        ctx.fillRect(15, 6, 2, 8);
+        ctx.fillStyle = light;
+        ctx.fillRect(15, 6, 1, 8);
+
+        // Gem at top
         ctx.fillStyle = '#ffffff';
-        ctx.fillRect(15, 0, 2, 4);
+        ctx.fillRect(16, 1, 1, 1);
+
+        // Collar gems
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(12, 17, 1, 1);
+        ctx.fillRect(19, 17, 1, 1);
     }
 
     function drawKnight(ctx, color) {
-        // Base
+        var dark = shadeColor(color, -40);
+        var light = shadeColor(color, 40);
+
+        // Base platform
+        ctx.fillStyle = dark;
+        ctx.fillRect(7, 28, 18, 3);
         ctx.fillStyle = color;
-        ctx.fillRect(8, 26, 16, 4);
-        // Body
-        ctx.fillRect(10, 14, 12, 12);
-        // Head
-        ctx.fillRect(8, 6, 14, 8);
-        // Ear
-        ctx.fillRect(8, 2, 4, 6);
-        // Nose
-        ctx.fillRect(20, 8, 4, 4);
+        ctx.fillRect(8, 27, 16, 3);
+
+        // Body/neck
+        ctx.fillStyle = color;
+        ctx.fillRect(10, 18, 12, 9);
+        ctx.fillStyle = dark;
+        ctx.fillRect(10, 18, 2, 9);
+        ctx.fillStyle = light;
+        ctx.fillRect(14, 18, 3, 9);
+
+        // Neck (angled)
+        ctx.fillStyle = color;
+        ctx.fillRect(9, 15, 10, 3);
+
+        // Head main shape
+        ctx.fillStyle = color;
+        ctx.fillRect(7, 7, 16, 8);
+        ctx.fillStyle = dark;
+        ctx.fillRect(7, 7, 16, 1);
+        ctx.fillRect(7, 7, 1, 8);
+        ctx.fillStyle = light;
+        ctx.fillRect(10, 9, 4, 4);
+
+        // Ears (two pointed)
+        ctx.fillStyle = color;
+        ctx.fillRect(7, 3, 3, 5);
+        ctx.fillRect(11, 2, 2, 6);
+        ctx.fillStyle = dark;
+        ctx.fillRect(7, 3, 1, 5);
+        ctx.fillRect(11, 2, 1, 6);
+
+        // Mane (back of head)
+        ctx.fillStyle = dark;
+        ctx.fillRect(20, 8, 3, 6);
+        ctx.fillRect(21, 6, 2, 2);
+
+        // Snout/muzzle
+        ctx.fillStyle = color;
+        ctx.fillRect(20, 10, 5, 4);
+        ctx.fillStyle = dark;
+        ctx.fillRect(20, 10, 5, 1);
+        ctx.fillStyle = light;
+        ctx.fillRect(21, 12, 3, 2);
+
+        // Nostril
+        ctx.fillStyle = dark;
+        ctx.fillRect(23, 11, 1, 1);
+
         // Eye
         ctx.fillStyle = '#ffffff';
-        ctx.fillRect(16, 8, 2, 2);
+        ctx.fillRect(14, 9, 2, 2);
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(15, 9, 1, 1);
     }
 
     function drawPawn(ctx, color) {
-        // Base
+        var dark = shadeColor(color, -40);
+        var light = shadeColor(color, 40);
+
+        // Base platform
+        ctx.fillStyle = dark;
+        ctx.fillRect(8, 28, 16, 3);
         ctx.fillStyle = color;
-        ctx.fillRect(10, 26, 12, 4);
-        // Body
-        ctx.fillRect(12, 18, 8, 8);
+        ctx.fillRect(9, 27, 14, 3);
+
+        // Lower body
+        ctx.fillStyle = color;
+        ctx.fillRect(10, 22, 12, 5);
+        ctx.fillStyle = dark;
+        ctx.fillRect(10, 22, 2, 5);
+        ctx.fillRect(20, 22, 2, 5);
+
+        // Waist/collar
+        ctx.fillStyle = dark;
+        ctx.fillRect(11, 20, 10, 2);
+
+        // Upper body (tapered)
+        ctx.fillStyle = color;
+        ctx.fillRect(12, 14, 8, 6);
+        ctx.fillStyle = dark;
+        ctx.fillRect(12, 14, 1, 6);
+        ctx.fillRect(19, 14, 1, 6);
+        ctx.fillStyle = light;
+        ctx.fillRect(15, 14, 2, 6);
+
+        // Neck
+        ctx.fillStyle = color;
+        ctx.fillRect(13, 12, 6, 2);
+
         // Head
-        ctx.fillRect(11, 12, 10, 6);
-        // Top
-        ctx.fillRect(12, 8, 8, 4);
-        // Highlight
-        ctx.fillStyle = '#ffffff';
-        ctx.fillRect(14, 10, 2, 2);
+        ctx.fillStyle = color;
+        ctx.fillRect(11, 6, 10, 6);
+        ctx.fillStyle = dark;
+        ctx.fillRect(11, 6, 10, 1);
+        ctx.fillRect(11, 6, 1, 6);
+        ctx.fillStyle = light;
+        ctx.fillRect(14, 7, 3, 4);
+
+        // Head top (rounded)
+        ctx.fillStyle = color;
+        ctx.fillRect(12, 4, 8, 2);
+        ctx.fillRect(13, 3, 6, 1);
+        ctx.fillRect(14, 2, 4, 1);
+
+        // Highlight on head
+        ctx.fillStyle = light;
+        ctx.fillRect(13, 4, 2, 1);
+    }
+
+    // Helper: shade a hex color
+    function shadeColor(hex, amount) {
+        var num = parseInt(hex.replace('#', ''), 16);
+        var r = Math.min(255, Math.max(0, (num >> 16) + amount));
+        var g = Math.min(255, Math.max(0, ((num >> 8) & 0x00FF) + amount));
+        var b = Math.min(255, Math.max(0, (num & 0x0000FF) + amount));
+        return '#' + (0x1000000 + r * 0x10000 + g * 0x100 + b).toString(16).slice(1);
     }
 
     // ── Event Handlers ───────────────────────────────────────────────────────
